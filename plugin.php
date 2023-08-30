52a90cb34153a95ccaceb352bb9f3c91304093ea
devadob <admir.obralija@tik.uni-stuttgart.de>
2023-08-30 13:45:03 +0200

    Update ILIAS max version.

--- plugin.php
+++ plugin.php
@@ -3,6 +3,6 @@
 $id = "assjupyter";
 $version = "1.0.0";
 $ilias_min_version = "8.0.0";
-$ilias_max_version = "8.999";
+$ilias_max_version = "8.999.999";
 $responsible = "Admir Obralija";
 $responsible_mail = "admir.obralija@tik.uni-stuttgart.de";
